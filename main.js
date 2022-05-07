9e5e00abc58e4ca37ebffb4ef3821cf696217381
Sunny1135 <100459411+Sunny1135@users.noreply.github.com>
2022-05-07 19:52:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+nose_x="";
+nose_y="";
+function preload() {
+  clown_nose =loadImage('https://postimg.cc/bsV5tR30');
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video=createCapture(VIDEO);
+  video.size(300,300);
+  video.hide();
+  poseNet=ml5.poseNet(video,modelLoaded);
+  poseNet.on('pose',gotPoses)
+}
+function modelLoaded()
+{
+console.log('PoseNet Is Initialized');
+}
+function draw() {
+  image(video,0,0,300,300);  
+}
+
+function take_snapshot(){    
+  save('mymustache.png');
+}
+function gotPoses(results)
+{
+if(results.lenght>0)
+{
+console.log(results);
+console.log("nose x="+results[0].pose.nose.x);
+console.log("nose y="+results[0].pose.nose.y);
+console.log("nose x="+nose_x);
+console.log("nose y="+nose_y);
+}
+}
